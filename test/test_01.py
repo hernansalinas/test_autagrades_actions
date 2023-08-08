1afbb9bb7019acf6feb2fead382b2466b2d5aa7f
hernansalinas <61880024+hernansalinas@users.noreply.github.com>
2023-08-08 16:04:34 -0500

    Update test_01.py

--- test/test_01.py
+++ test/test_01.py
@@ -1,3 +1,5 @@
+import pytest
+
 @pytest.mark.parametrize("n, resultado", [(5, 15), (6, 48), (7, 105), (8, 384)])
 def test_doble_factorial(n, resultado):
     assert doble_factorial(n) == resultado
