57a92bb953df8b50eb60c83c0df35b0e8f8ffc14
hernansalinas <61880024+hernansalinas@users.noreply.github.com>
2023-08-08 17:51:38 -0500

    Update actividad_01.py

--- Students/Salinas_431/actividad_01.py
+++ Students/Salinas_431/actividad_01.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 ## Definir la función que calcula el doble factorial
 def doble_factorial(n):
-  # Verificar que n sea un número entero positivo
-  if not isinstance(n, int) or n < 0:
-    raise ValueError("n debe ser un número entero positivo")
-  # Inicializar el resultado con el valor de n
-  resultado = n
-  # Usar un bucle while para ir multiplicando los números con la misma paridad que n
-  while n > 1:
-    # Restar dos unidades a n
-    n -= 2
-    # Multiplicar el resultado por n
-    resultado *= n
-  # Devolver el resultado
-  return resultado
+    # Verificar que n sea un entero positivo
+    if not isinstance(n, int) or n < 0:
+        return None
+    # Inicializar el acumulador con 1
+    resultado = 1
+    # Iterar mientras n sea mayor o igual a 1
+    while n >= 1:
+        # Multiplicar el acumulador por n
+        resultado = resultado * n
+        # Restar 2 a n
+        n = n - 2
+    # Devolver el acumulador
+    return resultado
 
