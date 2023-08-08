9d367c167266888a75dbb05f99e7cc1822dc09bf
hernansalinas <61880024+hernansalinas@users.noreply.github.com>
2023-08-08 17:42:45 -0500

    Update test_01.py

--- test/test_01.py
+++ test/test_01.py
@@ -1,6 +1,6 @@
 import pytest
 #from actividad_01 import doble_factorial
-from test_autagrades_actions.Students.Salinas_431.actividad_01 import doble_factorial
+from actividad_01 import doble_factorial
 
 @pytest.mark.parametrize("n, resultado", [(5, 15), (6, 48), (7, 105), (8, 384)])
 def test_doble_factorial(n, resultado):
